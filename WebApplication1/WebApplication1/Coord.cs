bab4737761424a5429617e3dd5b023dbd38f2d83
Lorenzo <lorenzopica31@gmail.com>
2025-04-15 17:28:59 +0200

    Created game grid(with coordinates x and y) and snake head

---
+++ WebApplication1/WebApplication1/Coord.cs
@@ -0,0 +1,23 @@
+using System;
+
+
+ namespace Snake
+   {
+
+      internal class Coord{
+
+        private int x;
+        private int y;
+
+        public int X { get{ return x; } }
+
+        public int Y { get{ return y; } }
+
+        public Coord(int x, int y){ 
+            this.x = x;
+            this.y = Y;
+
+        }
+
+      }
+    }
