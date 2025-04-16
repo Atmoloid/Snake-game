dfa0678b3bf62fb10e1fb1bb029883b9b7eac117
Lorenzo <lorenzopica31@gmail.com>
2025-04-16 19:17:32 +0200

    bug fix on public override

--- WebApplication1/WebApplication1/Coord.cs
+++ WebApplication1/WebApplication1/Coord.cs
@@ -19,7 +19,7 @@ using System;
 
         }
 
-        public override bool Equals(object? obj)
+        /*public override bool Equals(object? obj)
         {
           if((obj == null) || !GetType().Equals(obj.GetType())){ 
                 return false;
@@ -28,6 +28,20 @@ using System;
                 Coord other = (Coord)obj;
                 return other.x && y == other.y;
            }
+        }*/
+        public override bool Equals(object? obj)
+        {
+            // Check for null and type mismatch first
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            // Safely cast the object to Coord
+            Coord other = (Coord)obj;
+
+            // Compare the properties for equality
+            return x == other.x && y == other.y; // Corrected comparison
         }
 
       }
